1cd7a26cc8fec7141c53731f1b4980581efb3e4b
ErnestoIRamirez <nenoirm27@gmail.com>
2018-05-29 23:26:06 -0500

    modifique todo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel asdasd
                 </div>
 
                 <div class="links">
